52d33691b777d41258a1cddb9499e98a50e94dad
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-05-19 18:32:17 -0500

    media: siano: Replace one-element array with flexible-array
    The current codebase makes use of one-element arrays in the following
    form:
    
    struct something {
        int length;
        u8 data[1];
    };
    
    struct something *instance;
    
    instance = kmalloc(sizeof(*instance) + size, GFP_KERNEL);
    instance->length = size;
    memcpy(instance->data, source, size);
    
    but the preferred mechanism to declare variable-length types such as
    these ones is a flexible array member[1][2], introduced in C99:
    
    struct foo {
            int stuff;
            struct boo array[];
    };
    
    By making use of the mechanism above, we will get a compiler warning
    in case the flexible array does not occur last in the structure, which
    will help us prevent some kind of undefined behavior bugs from being
    inadvertently introduced[3] to the codebase from now on.
    
    This issue was found with the help of Coccinelle.
    
    [1] https://gcc.gnu.org/onlinedocs/gcc/Zero-Length.html
    [2] https://github.com/KSPP/linux/issues/21
    [3] commit 76497732932f ("cxgb3/l2t: Fix undefined behaviour")
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/media/common/siano/smscoreapi.h
+++ drivers/media/common/siano/smscoreapi.h
@@ -621,7 +621,7 @@ struct sms_msg_hdr {
 
 struct sms_msg_data {
 	struct sms_msg_hdr x_msg_header;
-	u32 msg_data[1];
+	u32 msg_data[];
 };
 
 struct sms_msg_data2 {
@@ -671,7 +671,7 @@ struct sms_firmware {
 	u32			check_sum;
 	u32			length;
 	u32			start_address;
-	u8			payload[1];
+	u8			payload[];
 };
 
 /* statistics information returned as response for
@@ -1051,13 +1051,13 @@ struct sms_i2c_req {
 	u32	device_address; /* I2c device address */
 	u32	write_count; /* number of bytes to write */
 	u32	read_count; /* number of bytes to read */
-	u8	Data[1];
+	u8	Data[];
 };
 
 struct sms_i2c_res {
 	u32	status; /* non-zero value in case of failure */
 	u32	read_count; /* number of bytes read */
-	u8	Data[1];
+	u8	Data[];
 };
 
 
