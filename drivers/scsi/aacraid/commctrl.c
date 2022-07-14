8705776d89180b9e76c5ce506212b2e678150c46
Gustavo A. R. Silva <gustavoars@kernel.org>
2022-07-13 22:24:14 -0500

    scsi: aacraid: Replace one-element array with flexible-array member sgmap
    Replace one-element array with flexible-array member in struct
    sgmap and refactor the rest of the code, accordingly.
    
    This helps with the ongoing efforts to tighten the FORTIFY_SOURCE
    routines on memcpy().
    
    This issue was found with the help of Coccinelle and audited and fixed,
    manually.
    
    Link: https://github.com/KSPP/linux/issues/79
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/scsi/aacraid/commctrl.c
+++ drivers/scsi/aacraid/commctrl.c
@@ -561,7 +561,7 @@ static int aac_send_raw_srb(struct aac_dev* dev, void __user * arg)
 		rcode = -EINVAL;
 		goto cleanup;
 	}
-	actual_fibsize = sizeof(struct aac_srb) - sizeof(struct sgentry) +
+	actual_fibsize = sizeof(struct aac_srb) +
 		((user_srbcmd->sg.count & 0xff) * sizeof(struct sgentry));
 	actual_fibsize64 = actual_fibsize + (user_srbcmd->sg.count & 0xff) *
 	  (sizeof(struct sgentry64) - sizeof(struct sgentry));
