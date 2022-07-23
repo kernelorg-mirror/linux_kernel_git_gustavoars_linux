0e8d7435b1f333ce78cebf9834b8db43d0ce9f64
Gustavo A. R. Silva <gustavoars@kernel.org>
2022-07-23 15:26:55 -0500

    media: venus: hfi_cmds.h: Use struct_size() in code related to struct hfi_session_set_buffers_pkt
    Prefer struct_size() over open-coded versions of idiom:
    
        sizeof(struct-with-flex-array) + sizeof(type-of-flex-array) * count
    
    where count is the max number of items the flexible array is supposed to
    have.
    
    Link: https://github.com/KSPP/linux/issues/160
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/media/platform/qcom/venus/hfi_cmds.c
+++ drivers/media/platform/qcom/venus/hfi_cmds.c
@@ -200,8 +200,7 @@ int pkt_session_set_buffers(struct hfi_session_set_buffers_pkt *pkt,
 		}
 	} else {
 		pkt->extradata_size = 0;
-		pkt->shdr.hdr.size = sizeof(*pkt) +
-			(bd->num_buffers * sizeof(u32));
+		pkt->shdr.hdr.size = struct_size(pkt, buffer_info, bd->num_buffers);
 		for (i = 0; i < pkt->num_buffers; i++)
 			pkt->buffer_info[i] = bd->device_addr;
 	}
@@ -242,8 +241,8 @@ int pkt_session_unset_buffers(struct hfi_session_release_buffer_pkt *pkt,
 
 		pkt->extradata_size = 0;
 		pkt->shdr.hdr.size =
-				sizeof(struct hfi_session_set_buffers_pkt) +
-				(bd->num_buffers * sizeof(u32));
+				struct_size((struct hfi_session_set_buffers_pkt *)0,
+						buffer_info, bd->num_buffers);
 	}
 
 	pkt->response_req = bd->response_required;
