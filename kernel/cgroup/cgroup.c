7c9abd83f6b3412752cbb1a76954568b5316ad61
Gustavo A. R. Silva <gustavoars@kernel.org>
2024-03-01 16:53:21 -0600

    cgroup: Avoid -Wflex-array-member-not-at-end warnings
    -Wflex-array-member-not-at-end is coming in GCC-14, and we are getting
    ready to enable it globally.
    
    There is currently an object (`cgrp`) in the middle of `struct cgroup_root`
    that contains a flexible structure (`struct cgroup`):
    
    struct cgroup_root {
    
    	...
    
            struct cgroup cgrp;
    
            /* must follow cgrp for cgrp->ancestors[0], see above */
            struct cgroup *cgrp_ancestor_storage;
    
    	...
    };
    
    So, in order to avoid ending up with a flexible-array member in the middle
    of a struct, we use the `struct_group_tagged()` helper to separate the
    flexible array from the rest of the members in the flexible structure:
    
    struct cgroup {
            struct_group_tagged(cgroup_hdr, hdr,
    
    		... the rest of the members
    
            );
            /* All ancestors including self */
            struct cgroup *ancestors[];
    };
    
    With the change described above, we can now declare objects of the type
    of the tagged struct, without embedding flexible arrays in the middle of
    another struct:
    
    struct cgroup_root {
    
            ...
    
            struct cgroup_hdr cgrp;
    
            /* must follow cgrp for cgrp->ancestors[0], see above */
            struct cgroup *cgrp_ancestor_storage;
    
            ...
    };
    
    We also use `container_of()` whenever we need to retrieve a pointer to
    the flexible structure `struct cgroup`, through which flexible array
    `ancestors` could be accessed.
    
    Only in the case of a static initialization, a direct cast to
    `struct cgroup *` is used.
    
    So, with these changes, fix _thousands_ of the following warnings:
    
    ./include/linux/cgroup-defs.h:574:23: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
      574 |         struct cgroup cgrp;
          |                       ^~~~
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -744,7 +744,7 @@ struct css_set init_css_set = {
 	 * statically too so that the default cgroup can be accessed safely
 	 * early during boot.
 	 */
-	.dfl_cgrp		= &cgrp_dfl_root.cgrp,
+	.dfl_cgrp		= (struct cgroup *)&cgrp_dfl_root.cgrp,
 };
 
 static int css_set_count	= 1;	/* 1 for init_css_set */
@@ -1320,12 +1320,13 @@ void cgroup_free_root(struct cgroup_root *root)
 
 static void cgroup_destroy_root(struct cgroup_root *root)
 {
-	struct cgroup *cgrp = &root->cgrp;
+	struct cgroup *cgrp = container_of(&root->cgrp, struct cgroup, hdr);
 	struct cgrp_cset_link *link, *tmp_link;
 
 	trace_cgroup_destroy_root(root);
 
-	cgroup_lock_and_drain_offline(&cgrp_dfl_root.cgrp);
+	cgroup_lock_and_drain_offline(container_of(&cgrp_dfl_root.cgrp,
+						   struct cgroup, hdr));
 
 	BUG_ON(atomic_read(&root->nr_cgrps));
 	BUG_ON(!list_empty(&cgrp->self.children));
@@ -1372,7 +1373,7 @@ static inline struct cgroup *__cset_cgroup_from_root(struct css_set *cset,
 	struct cgroup *res_cgroup = NULL;
 
 	if (cset == &init_css_set) {
-		res_cgroup = &root->cgrp;
+		res_cgroup = container_of(&root->cgrp, struct cgroup, hdr);
 	} else if (root == &cgrp_dfl_root) {
 		res_cgroup = cset->dfl_cgrp;
 	} else {
@@ -1453,7 +1454,7 @@ static struct cgroup *current_cgns_cgroup_dfl(void)
 		 * nsproxy == NULL. Fall back to cgrp_dfl_root which will make all
 		 * cgroups visible for lookups.
 		 */
-		return &cgrp_dfl_root.cgrp;
+		return container_of(&cgrp_dfl_root.cgrp, struct cgroup, hdr);
 	}
 }
 
@@ -1780,7 +1781,8 @@ err:
 
 int rebind_subsystems(struct cgroup_root *dst_root, u16 ss_mask)
 {
-	struct cgroup *dcgrp = &dst_root->cgrp;
+	struct cgroup *dcgrp = container_of(&dst_root->cgrp,
+					    struct cgroup, hdr);
 	struct cgroup_subsys *ss;
 	int ssid, ret;
 	u16 dfl_disable_ss_mask = 0;
@@ -1793,8 +1795,10 @@ int rebind_subsystems(struct cgroup_root *dst_root, u16 ss_mask)
 		 * If @ss is an implicit controller, it is exempt from this
 		 * rule and can be stolen.
 		 */
-		if (css_next_child(NULL, cgroup_css(&ss->root->cgrp, ss)) &&
-		    !ss->implicit_on_dfl)
+		if (css_next_child(NULL,
+			cgroup_css(container_of(&ss->root->cgrp,
+						struct cgroup, hdr), ss)) &&
+				!ss->implicit_on_dfl)
 			return -EBUSY;
 
 		/* can't move between two non-dummy roots either */
@@ -1811,7 +1815,8 @@ int rebind_subsystems(struct cgroup_root *dst_root, u16 ss_mask)
 	} while_each_subsys_mask();
 
 	if (dfl_disable_ss_mask) {
-		struct cgroup *scgrp = &cgrp_dfl_root.cgrp;
+		struct cgroup *scgrp = container_of(&cgrp_dfl_root.cgrp,
+						    struct cgroup, hdr);
 
 		/*
 		 * Controllers from default hierarchy that need to be rebound
@@ -1824,7 +1829,7 @@ int rebind_subsystems(struct cgroup_root *dst_root, u16 ss_mask)
 
 	do_each_subsys_mask(ss, ssid, ss_mask) {
 		struct cgroup_root *src_root = ss->root;
-		struct cgroup *scgrp = &src_root->cgrp;
+		struct cgroup *scgrp = container_of(&src_root->cgrp, struct cgroup, hdr);
 		struct cgroup_subsys_state *css = cgroup_css(scgrp, ss);
 		struct css_set *cset, *cset_pos;
 		struct css_task_iter *it;
@@ -2065,7 +2070,7 @@ static void init_cgroup_housekeeping(struct cgroup *cgrp)
 void init_cgroup_root(struct cgroup_fs_context *ctx)
 {
 	struct cgroup_root *root = ctx->root;
-	struct cgroup *cgrp = &root->cgrp;
+	struct cgroup *cgrp = container_of(&root->cgrp, struct cgroup, hdr);
 
 	INIT_LIST_HEAD_RCU(&root->root_list);
 	atomic_set(&root->nr_cgrps, 1);
@@ -2085,7 +2090,7 @@ void init_cgroup_root(struct cgroup_fs_context *ctx)
 int cgroup_setup_root(struct cgroup_root *root, u16 ss_mask)
 {
 	LIST_HEAD(tmp_links);
-	struct cgroup *root_cgrp = &root->cgrp;
+	struct cgroup *root_cgrp = container_of(&root->cgrp, struct cgroup, hdr);
 	struct kernfs_syscall_ops *kf_sops;
 	struct css_set *cset;
 	int i, ret;
@@ -2225,7 +2230,7 @@ int cgroup_do_get_tree(struct fs_context *fc)
 	}
 
 	if (!ctx->kfc.new_sb_created)
-		cgroup_put(&ctx->root->cgrp);
+		cgroup_put(container_of(&ctx->root->cgrp, struct cgroup, hdr));
 
 	return ret;
 }
@@ -2250,7 +2255,7 @@ static int cgroup_get_tree(struct fs_context *fc)
 	int ret;
 
 	WRITE_ONCE(cgrp_dfl_visible, true);
-	cgroup_get_live(&cgrp_dfl_root.cgrp);
+	cgroup_get_live(container_of(&cgrp_dfl_root.cgrp, struct cgroup, hdr));
 	ctx->root = &cgrp_dfl_root;
 
 	ret = cgroup_do_get_tree(fc);
@@ -2315,10 +2320,11 @@ static void cgroup_kill_sb(struct super_block *sb)
 	 */
 	if (list_empty(&root->cgrp.self.children) && root != &cgrp_dfl_root &&
 	    !percpu_ref_is_dying(&root->cgrp.self.refcnt)) {
-		cgroup_bpf_offline(&root->cgrp);
+		cgroup_bpf_offline(container_of(&root->cgrp, struct cgroup,
+						hdr));
 		percpu_ref_kill(&root->cgrp.self.refcnt);
 	}
-	cgroup_put(&root->cgrp);
+	cgroup_put(container_of(&root->cgrp, struct cgroup, hdr));
 	kernfs_kill_sb(sb);
 }
 
@@ -4320,7 +4326,7 @@ restart:
 static int cgroup_apply_cftypes(struct cftype *cfts, bool is_add)
 {
 	struct cgroup_subsys *ss = cfts[0].ss;
-	struct cgroup *root = &ss->root->cgrp;
+	struct cgroup *root = container_of(&ss->root->cgrp, struct cgroup, hdr);
 	struct cgroup_subsys_state *css;
 	int ret = 0;
 
@@ -6074,7 +6080,8 @@ static void __init cgroup_init_subsys(struct cgroup_subsys *ss, bool early)
 	css = ss->css_alloc(NULL);
 	/* We don't handle early failures gracefully */
 	BUG_ON(IS_ERR(css));
-	init_and_link_css(css, ss, &cgrp_dfl_root.cgrp);
+	init_and_link_css(css, ss, container_of(&cgrp_dfl_root.cgrp,
+						struct cgroup, hdr));
 
 	/*
 	 * Root csses are never destroyed and we can't initialize
@@ -7051,7 +7058,7 @@ void cgroup_sk_alloc(struct sock_cgroup_data *skcd)
 	rcu_read_lock();
 	/* Don't associate the sock with unrelated interrupted task's cgroup. */
 	if (in_interrupt()) {
-		cgroup = &cgrp_dfl_root.cgrp;
+		cgroup = container_of(&cgrp_dfl_root.cgrp, struct cgroup, hdr);
 		cgroup_get(cgroup);
 		goto out;
 	}
