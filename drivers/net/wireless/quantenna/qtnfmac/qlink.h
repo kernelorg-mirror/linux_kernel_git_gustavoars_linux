351d4186d8097cc9e5e5f15fd552df583a3ff3fd
Gustavo A. R. Silva <gustavoars@kernel.org>
2024-03-28 14:09:11 -0600

    qtnfmac: Avoid -Wflex-array-member-not-at-end warnings
    TEST COMMIT.
    
    ADD NOTES
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/net/wireless/quantenna/qtnfmac/qlink.h
+++ drivers/net/wireless/quantenna/qtnfmac/qlink.h
@@ -1510,8 +1510,11 @@ enum qlink_tlv_id {
 };
 
 struct qlink_tlv_hdr {
-	__le16 type;
-	__le16 len;
+	/* New members must be added within the struct_group() macro below. */
+	__struct_group(qlink_tlv_hdr_hdr, hdr, __packed,
+		__le16 type;
+		__le16 len;
+	);
 	u8 val[];
 } __packed;
 
@@ -1567,7 +1570,7 @@ enum qlink_reg_rule_flags {
  * @dfs_cac_ms: DFS CAC period.
  */
 struct qlink_tlv_reg_rule {
-	struct qlink_tlv_hdr hdr;
+	struct qlink_tlv_hdr_hdr hdr;
 	__le32 start_freq_khz;
 	__le32 end_freq_khz;
 	__le32 max_bandwidth_khz;
@@ -1606,7 +1609,7 @@ enum qlink_dfs_state {
  * @channel: ieee80211 channel settings.
  */
 struct qlink_tlv_channel {
-	struct qlink_tlv_hdr hdr;
+	struct qlink_tlv_hdr_hdr hdr;
 	struct qlink_channel chan;
 } __packed;
 
@@ -1618,7 +1621,7 @@ struct qlink_tlv_channel {
  * @chan: channel definition data.
  */
 struct qlink_tlv_chandef {
-	struct qlink_tlv_hdr hdr;
+	struct qlink_tlv_hdr_hdr hdr;
 	struct qlink_chandef chdef;
 } __packed;
 
@@ -1643,7 +1646,7 @@ enum qlink_ie_set_type {
  * @ie_data: IEs data.
  */
 struct qlink_tlv_ie_set {
-	struct qlink_tlv_hdr hdr;
+	struct qlink_tlv_hdr_hdr hdr;
 	u8 type;
 	u8 flags;
 	u8 rsvd[2];
@@ -1657,7 +1660,7 @@ struct qlink_tlv_ie_set {
  * @ie_data: IEs data.
  */
 struct qlink_tlv_ext_ie {
-	struct qlink_tlv_hdr hdr;
+	struct qlink_tlv_hdr_hdr hdr;
 	u8 eid_ext;
 	u8 rsvd[3];
 	u8 ie_data[];
@@ -1678,7 +1681,7 @@ struct qlink_sband_iftype_data {
  * @iftype_data: interface type data entries.
  */
 struct qlink_tlv_iftype_data {
-	struct qlink_tlv_hdr hdr;
+	struct qlink_tlv_hdr_hdr hdr;
 	u8 n_iftype_data;
 	u8 rsvd[3];
 	struct qlink_sband_iftype_data iftype_data[];
