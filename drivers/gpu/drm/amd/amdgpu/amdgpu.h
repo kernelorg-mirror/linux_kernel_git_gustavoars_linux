2cdd2d8977638e3892a38d2615ef9a3b04f5a472
Gustavo A. R. Silva <gustavoars@kernel.org>
2024-06-11 10:48:22 -0600

    treewide_some: fix multiple -Wfamnae warnings that must be audited separately
    FIX the following errors so we can have a clean build. This is a
    temporary workaround and the code should be audited and properly fixed
    up later.
    
    ./include/linux/bpf.h:1990:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/asm-generic/hyperv-tlfs.h:494:25: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/hyperv.h:767:38: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/kvm_host.h:1842:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/bpf.h:1990:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/asm-generic/hyperv-tlfs.h:494:25: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/hyperv.h:767:38: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/kvm_host.h:1842:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/bpf.h:1990:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/asm-generic/hyperv-tlfs.h:494:25: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ...+./include/linux/hyperv.h:767:38: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ..........+...../include/linux/kvm_host.h:1842:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/bpf.h:1990:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/bpf.h:1990:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.h:109:28: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.h:109:28: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    ./include/linux/bpf.h:1990:31: error: structure containing a flexible array member is not at the end of another structure [-Werror=flex-array-member-not-at-end]
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -1041,8 +1041,6 @@ struct amdgpu_device {
 	/* for userq and VM fences */
 	struct amdgpu_seq64		seq64;
 
-	/* KFD */
-	struct amdgpu_kfd_dev		kfd;
 
 	/* UMC */
 	struct amdgpu_umc		umc;
@@ -1175,6 +1173,9 @@ struct amdgpu_device {
 	bool                            debug_disable_soft_recovery;
 	bool                            debug_use_vram_fw_buf;
 	bool                            debug_enable_ras_aca;
+
+	/* KFD */
+	struct amdgpu_kfd_dev           kfd;
 };
 
 static inline uint32_t amdgpu_ip_version(const struct amdgpu_device *adev,
