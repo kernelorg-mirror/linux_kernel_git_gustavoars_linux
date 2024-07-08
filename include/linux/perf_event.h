eeb0fbc69eb06524652d67514b638e81e22d0597
Gustavo A. R. Silva <gustavoars@kernel.org>
2024-07-08 17:02:24 -0600

    perf: Avoid -Wflex-array-member-not-at-end warnings
    -Wflex-array-member-not-at-end was introduced in GCC-14, and we are
    getting ready to enable it, globally.
    
    So, in order to avoid ending up with a flexible-array member in the
    middle of multiple other structs, we use the `struct_group_tagged()`
    helper to create a new tagged `struct perf_branch_stack_hdr`.
    This structure groups together all the members of the flexible
    `struct perf_branch_stack` except the flexible array.
    
    As a result, the array is effectively separated from the rest of the
    members without modifying the memory layout of the flexible structure.
    We then change the type of the middle struct members currently causing
    trouble from `struct perf_branch_stack` to `struct perf_branch_stack_hdr`.
    
    We also want to ensure that when new members need to be added to the
    flexible structure, they are always included within the newly created
    tagged struct. For this, we use `static_assert()`. This ensures that the
    memory layout for both the flexible structure and the new tagged struct
    is the same after any changes.
    
    This approach avoids having to implement `struct perf_branch_stack_hdr`
    as a completely separate structure, thus preventing having to maintain
    two independent but basically identical structures, closing the door
    to potential bugs in the future.
    
    We also use `container_of()` whenever we need to retrieve a pointer to
    the flexible structure, through which the flexible-array member can be
    accessed, if necessary.
    
    So, with these changes, fix the following warnings:
    arch/x86/events/amd/../perf_event.h:289:41: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    arch/x86/events/intel/../perf_event.h:289:41: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    arch/x86/events/perf_event.h:289:41: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    arch/x86/events/zhaoxin/../perf_event.h:289:41: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    ./arch/x86/include/generated/../../events/perf_event.h:289:41: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    arch/x86/xen/../events/perf_event.h:289:41: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -121,10 +121,15 @@ static __always_inline bool perf_raw_frag_last(const struct perf_raw_frag *frag)
  * already stored in age order, the hw_idx should be 0.
  */
 struct perf_branch_stack {
-	__u64				nr;
-	__u64				hw_idx;
+	/* New members MUST be added within the struct_group() macro below. */
+	struct_group_tagged(perf_branch_stack_hdr, hdr,
+		__u64				nr;
+		__u64				hw_idx;
+	);
 	struct perf_branch_entry	entries[];
 };
+static_assert(offsetof(struct perf_branch_stack, entries) == sizeof(struct perf_branch_stack_hdr),
+	      "struct member likely outside of struct_group_tagged()");
 
 struct task_struct;
 
