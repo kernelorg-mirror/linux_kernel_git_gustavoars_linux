536640837ae5c2952180b6354ced44acc536ad83
Gustavo A. R. Silva <gustavoars@kernel.org>
2024-10-22 11:57:56 -0600

    cgroup: Avoid -Wflex-array-member-not-at-end warnings
    -Wflex-array-member-not-at-end is coming in GCC-14, and we are getting
    ready to enable it globally.
    
    There is currently an object (`cgrp`) in the middle of `struct cgroup_root`
    that contains a flexible structure (`struct cgroup`):
    
    struct cgroup_root {
    
            ...
    
            struct cgroup cgrp;
    
            /* must follow cgrp for cgrp->ancestors[0], see above */
            struct cgroup *cgrp_ancestor_storage;
    
            ...
    };
    
    So, in order to avoid ending up with a flexible-array member in the middle
    of a struct, we use the `struct_group_tagged()` helper to separate the
    flexible array from the rest of the members in the flexible structure:
    
    struct cgroup {
            struct_group_tagged(cgroup_hdr, hdr,
    
                    ... the rest of the members
    
            );
            /* All ancestors including self */
            struct cgroup *ancestors[];
    };
    
    With the change described above, we can now declare objects of the type
    of the tagged struct, without embedding flexible arrays in the middle of
    another struct:
    
    struct cgroup_root {
    
            ...
    
            struct cgroup_hdr cgrp;
    
            /* must follow cgrp for cgrp->ancestors[0], see above */
            struct cgroup *cgrp_ancestor_storage;
    
            ...
    };
    
    We also use `container_of()` whenever we need to retrieve a pointer to
    the flexible structure `struct cgroup`, through which flexible array
    `ancestors` could be accessed.
    
    Only in the case of a static initialization, a direct cast to
    `struct cgroup *` is used.
    
    So, with these changes, fix _thousands_ of the following warnings:
    
    ./include/linux/cgroup-defs.h:574:23: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
      574 |         struct cgroup cgrp;
          |                       ^~~~
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -1092,7 +1092,7 @@ int cgroup1_reconfigure(struct fs_context *fc)
 	int ret = 0;
 	u16 added_mask, removed_mask;
 
-	cgroup_lock_and_drain_offline(&cgrp_dfl_root.cgrp);
+	cgroup_lock_and_drain_offline(container_of(&cgrp_dfl_root.cgrp, struct cgroup, hdr));
 
 	/* See what subsystems are wanted */
 	ret = check_cgroupfs_options(fc);
@@ -1182,7 +1182,7 @@ static int cgroup1_root_to_use(struct fs_context *fc)
 
 		if (!percpu_ref_tryget_live(&ss->root->cgrp.self.refcnt))
 			return 1;	/* restart */
-		cgroup_put(&ss->root->cgrp);
+		cgroup_put(container_of(&ss->root->cgrp, struct cgroup, hdr));
 	}
 
 	for_each_root(root) {
@@ -1257,7 +1257,7 @@ int cgroup1_get_tree(struct fs_context *fc)
 	if (!ns_capable(ctx->ns->user_ns, CAP_SYS_ADMIN))
 		return -EPERM;
 
-	cgroup_lock_and_drain_offline(&cgrp_dfl_root.cgrp);
+	cgroup_lock_and_drain_offline(container_of(&cgrp_dfl_root.cgrp, struct cgroup, hdr));
 
 	ret = cgroup1_root_to_use(fc);
 	if (!ret && !percpu_ref_tryget_live(&ctx->root->cgrp.self.refcnt))
