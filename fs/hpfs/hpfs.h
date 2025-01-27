d4f243b51dabedaf75e63723ea618d9e5cc956d2
Gustavo A. R. Silva <gustavoars@kernel.org>
2025-01-27 15:24:58 +1030

    fs: hpfs: Avoid multiple -Wflex-array-member-not-at-end warnings
    -Wflex-array-member-not-at-end was introduced in GCC-14, and we are
    getting ready to enable it, globally.
    
    So, in order to avoid ending up with a flexible-array member in the
    middle of other structs, we use the `struct_group_tagged()` helper
    to create a new tagged `struct bplus_header_fixed`. This structure
    groups together all the members of the flexible `struct bplus_header`
    except the flexible array.
    
    As a result, the array is effectively separated from the rest of the
    members without modifying the memory layout of the flexible structure.
    We then change the type of the middle struct member currently causing
    trouble from `struct bplus_header` to `struct bplus_header_fixed`.
    
    We also want to ensure that when new members need to be added to the
    flexible structure, they are always included within the newly created
    tagged struct. For this, we use `static_assert()`. This ensures that the
    memory layout for both the flexible structure and the new tagged struct
    is the same after any changes.
    
    This approach avoids having to implement `struct bplus_header_fixed`
    as a completely separate structure, thus preventing having to maintain
    two independent but basically identical structures, closing the door
    to potential bugs in the future.
    
    We also use `container_of()` whenever we need to retrieve a pointer to
    the flexible structure, through which we can access the flexible-array
    member, if necessary.
    
    So, with these changes, fix 26 of the following warnings:
    fs/hpfs/hpfs.h:456:23: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    fs/hpfs/hpfs.h:498:23: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- fs/hpfs/hpfs.h
+++ fs/hpfs/hpfs.h
@@ -396,18 +396,20 @@ enum {
 };
 struct bplus_header
 {
-  u8 flags;				/* bit 0 - high bit of first free entry offset
+	struct_group_tagged(bplus_header_fixed, __hdr,
+		u8 flags;		/* bit 0 - high bit of first free entry offset
 					   bit 5 - we're pointed to by an fnode,
 					   the data btree or some ea or the
 					   main ea bootage pointer ea_secno
 					   bit 6 - suggest binary search (unused)
 					   bit 7 - 1 -> (internal) tree of anodes
 						   0 -> (leaf) list of extents */
-  u8 fill[3];
-  u8 n_free_nodes;			/* free nodes in following array */
-  u8 n_used_nodes;			/* used nodes in following array */
-  __le16 first_free;			/* offset from start of header to
+		u8 fill[3];
+		u8 n_free_nodes;	/* free nodes in following array */
+		u8 n_used_nodes;	/* used nodes in following array */
+		__le16 first_free;	/* offset from start of header to
 					   first free node in array */
+  );
   union {
 	/* (internal) 2-word entries giving subtree pointers */
 	DECLARE_FLEX_ARRAY(struct bplus_internal_node, internal);
@@ -415,6 +417,8 @@ struct bplus_header
 	DECLARE_FLEX_ARRAY(struct bplus_leaf_node, external);
   } u;
 };
+static_assert(offsetof(struct bplus_header, u.internal) == sizeof(struct bplus_header_fixed),
+	      "struct member likely outside of __struct_group()");
 
 static inline bool bp_internal(struct bplus_header *bp)
 {
@@ -453,7 +457,7 @@ struct fnode
   __le16 flags;				/* bit 1 set -> ea_secno is an anode */
 					/* bit 8 set -> directory.  first & only extent
 					   points to dnode. */
-  struct bplus_header btree;		/* b+ tree, 8 extents or 12 subtrees */
+  struct bplus_header_fixed btree;	/* b+ tree, 8 extents or 12 subtrees */
   union {
     struct bplus_leaf_node external[8];
     struct bplus_internal_node internal[12];
@@ -495,7 +499,7 @@ struct anode
   __le32 self;				/* pointer to this anode */
   __le32 up;				/* parent anode or fnode */
 
-  struct bplus_header btree;		/* b+tree, 40 extents or 60 subtrees */
+  struct bplus_header_fixed btree;	/* b+tree, 40 extents or 60 subtrees */
   union {
     struct bplus_leaf_node external[40];
     struct bplus_internal_node internal[60];
