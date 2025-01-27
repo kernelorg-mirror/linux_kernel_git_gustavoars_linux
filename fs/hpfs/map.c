6ebc20b614045e5638083962060ba8fd2edb42f9
Gustavo A. R. Silva <gustavoars@kernel.org>
2025-01-27 15:24:58 +1030

    fs: hpfs: Avoid multiple -Wflex-array-member-not-at-end warnings
    -Wflex-array-member-not-at-end was introduced in GCC-14, and we are
    getting ready to enable it, globally.
    
    So, in order to avoid ending up with a flexible-array member in the
    middle of other structs, we use the `struct_group_tagged()` helper
    to create a new tagged `struct bplus_header_fixed`. This structure
    groups together all the members of the flexible `struct bplus_header`
    except the flexible array.
    
    As a result, the array is effectively separated from the rest of the
    members without modifying the memory layout of the flexible structure.
    We then change the type of the middle struct member currently causing
    trouble from `struct bplus_header` to `struct bplus_header_fixed`.
    
    We also want to ensure that when new members need to be added to the
    flexible structure, they are always included within the newly created
    tagged struct. For this, we use `static_assert()`. This ensures that the
    memory layout for both the flexible structure and the new tagged struct
    is the same after any changes.
    
    This approach avoids having to implement `struct bplus_header_fixed`
    as a completely separate structure, thus preventing having to maintain
    two independent but basically identical structures, closing the door
    to potential bugs in the future.
    
    We also use `container_of()` whenever we need to retrieve a pointer to
    the flexible structure, through which we can access the flexible-array
    member, if necessary.
    
    So, with these changes, fix 26 of the following warnings:
    fs/hpfs/hpfs.h:456:23: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    fs/hpfs/hpfs.h:498:23: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- fs/hpfs/map.c
+++ fs/hpfs/map.c
@@ -9,6 +9,9 @@
 
 #include "hpfs_fn.h"
 
+#define anode_btree	container_of(&anode->btree, struct bplus_header, __hdr)
+#define fnode_btree	container_of(&fnode->btree, struct bplus_header, __hdr)
+
 __le32 *hpfs_map_dnode_bitmap(struct super_block *s, struct quad_buffer_head *qbh)
 {
 	return hpfs_map_4sectors(s, hpfs_sb(s)->sb_dmap, qbh, 0);
@@ -178,14 +181,14 @@ struct fnode *hpfs_map_fnode(struct super_block *s, ino_t ino, struct buffer_hea
 			}
 			if (!fnode_is_dir(fnode)) {
 				if ((unsigned)fnode->btree.n_used_nodes + (unsigned)fnode->btree.n_free_nodes !=
-				    (bp_internal(&fnode->btree) ? 12 : 8)) {
+				    (bp_internal(fnode_btree) ? 12 : 8)) {
 					hpfs_error(s,
 					   "bad number of nodes in fnode %08lx",
 					    (unsigned long)ino);
 					goto bail;
 				}
 				if (le16_to_cpu(fnode->btree.first_free) !=
-				    8 + fnode->btree.n_used_nodes * (bp_internal(&fnode->btree) ? 8 : 12)) {
+				    8 + fnode->btree.n_used_nodes * (bp_internal(fnode_btree) ? 8 : 12)) {
 					hpfs_error(s,
 					    "bad first_free pointer in fnode %08lx",
 					    (unsigned long)ino);
@@ -233,12 +236,12 @@ struct anode *hpfs_map_anode(struct super_block *s, anode_secno ano, struct buff
 				goto bail;
 			}
 			if ((unsigned)anode->btree.n_used_nodes + (unsigned)anode->btree.n_free_nodes !=
-			    (bp_internal(&anode->btree) ? 60 : 40)) {
+			    (bp_internal(anode_btree) ? 60 : 40)) {
 				hpfs_error(s, "bad number of nodes in anode %08x", ano);
 				goto bail;
 			}
 			if (le16_to_cpu(anode->btree.first_free) !=
-			    8 + anode->btree.n_used_nodes * (bp_internal(&anode->btree) ? 8 : 12)) {
+			    8 + anode->btree.n_used_nodes * (bp_internal(anode_btree) ? 8 : 12)) {
 				hpfs_error(s, "bad first_free pointer in anode %08x", ano);
 				goto bail;
 			}
